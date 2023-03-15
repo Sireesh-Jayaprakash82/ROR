e2c6698097ab115e83e718121a7a6e44f976c71b
Sireesh Jayaprakash <sireesh.jayaprakash@happiestminds.com>
2023-03-15 10:58:32 +0530

    Commit

--- testapp/db/schema.rb
+++ testapp/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_103837) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_054634) do
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "review"
+    t.string "review"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- testapp/test/models/book_test.rb
+++ testapp/test/models/book_test.rb
@@ -2,10 +2,18 @@ require 'C:\Users\Sireesh.jayaprakash\OneDrive - Happiest Minds Technologies Lim
 
 class BookTest < ActiveSupport::TestCase
   fixtures :books
+
   def test_book
     one_book = Book.new :title => books(:one).title,
     :review => books(:one).review
 
     assert one_book.save
+
+    two_book = Book.new :title => books(:two).title,
+    :review => books(:two).review
+
+    assert two_book.save
+
+    assert_not_equal one_book.title, two_book.title
   end
 end
